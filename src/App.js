a61ba794f6661aecf9582ad0905d966dc0469009
Leonardo P Benevides <leonardobenevides@unifor.br>
2022-01-20 13:54:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import SearchInput from "./SearchInput";
+
+const api = "https://kitsu.io/api/edge/";
 
 function App() {
+  const [info, setInfo] = useState({});
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (text) {
+      setInfo({});
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
+        .then((response) => response.json())
+        .then((response) => {
+          setInfo(response);
+        });
+    }
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header-container">
+        <h1>Animes</h1>
+        <SearchInput value={text} onChange={(search) => setText(search)} />
+        {text && !info.data && (
+          <span>Carregando...</span>
+        )}
+      </div>
+      {info.data && (
+        <ul className="animes-list">
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+              <img src={anime.attributes.posterImage.small}/>
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
